66fc683d3e9b5be7cdf7c4c4e22f861e90762fe6
YasaswiniMancha <yasaswinigayathri@gmail.com>
2024-08-08 13:37:49 +0530

    updates file name

--- product_crud/src/main/java/com/crud/example/product/ProductCrudApplication.java
+++ product_crud/src/main/java/com/crud/example/product/ProductCrudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrudExample2Application {
+public class ProductCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudExample2Application.class, args);
+		SpringApplication.run(ProductCrudApplication.class, args);
 	}
 
 }
